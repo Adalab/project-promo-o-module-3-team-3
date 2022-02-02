a3a1f4024d291fbfb27f970e660f139a1c1f9cbf
IsaGarabana <isinhagb@gmail.com>
2022-02-02 12:46:45 +0100

    add route, API and LS

--- src/components/App.js
+++ src/components/App.js
@@ -1,81 +1,89 @@
 import "../styles/App.scss";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import postToApi from "../services/Api";
+import ls from "../services/LocalStorage";
 import Header from "./Header";
 import Footer from "./Footer";
 import Form from "./form/Form";
 import Preview from "./Preview";
 
 function App() {
-  const handleSubmit = (ev) => {
-    ev.preventDefault();
-  };
+	const handleSubmit = (ev) => {
+		ev.preventDefault();
+	};
 
-  // const [avatar, setAvatar] = useState("");
-  const [data, setData] = useState({
-    name: "",
-    job: "",
-    email: "",
-    phone: "",
-    linkedin: "",
-    github: "",
-    photo: "",
-    palette: "1",
-  });
-  {
-    /* Metemos en una constante el input sobre el que está actuando la usuaria con el ev.currentTarget.name. Llamamos a esa constante para cambiar el valor de la propiedad del objeto data */
-  }
-  const handleInput = (name, value) => {
-    const inputChanged = name;
-    setData({
-      ...data,
-      [inputChanged]: value,
-    });
-  };
-  {
-    /* Al hacer click en el reset, llamamos a handleReset que vacía todas las propiedades del objeto */
-  }
-  const handleReset = () => {
-    setData({
-      name: "",
-      job: "",
-      email: "",
-      phone: "",
-      linkedin: "",
-      github: "",
-      photo: "",
-      palette: "1",
-    });
-  };
+	// const [avatar, setAvatar] = useState("");
+	const [data, setData] = useState(
+    ls.get("data", {
+		name: "",
+		job: "",
+		email: "",
+		phone: "",
+		linkedin: "",
+		github: "",
+		photo: "",
+		palette: "1",
+	});
 
-  // const handleClick = (ev) =>{
-  //   const clickButton = ev.currentTarget;
+  useEffect(() => {
+    ls.set("data", data);
+  }, [data]);
 
-  //   // if (clickButton === )
 
-  const updateAvatar = (avatar) => {
-    setData({
-      ...data,
-      photo: avatar,
-    });
-  };
+	{
+		/* Metemos en una constante el input sobre el que está actuando la usuaria con el ev.currentTarget.name. Llamamos a esa constante para cambiar el valor de la propiedad del objeto data */
+	}
+	const handleInput = (name, value) => {
+		const inputChanged = name;
+		setData({
+			...data,
+			[inputChanged]: value,
+		});
+	};
+	{
+		/* Al hacer click en el reset, llamamos a handleReset que vacía todas las propiedades del objeto */
+	}
+	const handleReset = () => {
+		setData({
+			name: "",
+			job: "",
+			email: "",
+			phone: "",
+			linkedin: "",
+			github: "",
+			photo: "",
+			palette: "1",
+		});
+	};
 
-  return (
-    <div>
-      <main className="cards__main--wrapper">
-        <Header />
-        <section className="card__wrapper">
-          <Preview data={data} handleReset={handleReset} />
-          <Form
-            updateAvatar={updateAvatar}
-            data={data}
-            handleInput={handleInput}
-          />
-        </section>
-        <Footer />
-      </main>
-    </div>
-  );
+	// const handleClick = (ev) =>{
+	//   const clickButton = ev.currentTarget;
+
+	//   // if (clickButton === )
+
+	const updateAvatar = (avatar) => {
+		setData({
+			...data,
+			photo: avatar,
+		});
+	};
+
+	return (
+		<div>
+      <Switch>
+			<main className="cards__main--wrapper">
+				<Header />
+				<section className="card__wrapper">
+					<Preview data={data} handleReset={handleReset} />
+					<Form updateAvatar={updateAvatar} data={data} handleInput={handleInput} />
+				</section>
+				<Footer />
+			</main>
+      </Switch>
+		</div>
+	);
 }
 
 export default App;
