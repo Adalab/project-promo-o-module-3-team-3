39f81480c52029656f7f74ba6db7dc1dd2dd1704
Babsrm <babsruizm@gmail.com>
2022-02-03 11:39:36 +0100

    format and indent files

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
-import Logo from "../images/dont-be-undefined-logo.svg";
-import Footer from "./Footer";
+import { Link } from 'react-router-dom';
+import Logo from '../images/dont-be-undefined-logo.svg';
+import Footer from './Footer';
 //import Header from "./Header";
 
 const Landing = () => {
--- src/services/Api.js
+++ src/services/Api.js
@@ -1,8 +1,8 @@
 const postToApi = (userData) => {
-	return fetch("https://awesome-profile-cards.herokuapp.com/card", {
-		method: "POST",
+	return fetch('https://awesome-profile-cards.herokuapp.com/card', {
+		method: 'POST',
 		body: JSON.stringify(userData),
-		headers: { "Content-Type": "application/json" },
+		headers: { 'Content-Type': 'application/json' },
 	}).then((response) => response.json());
 };
 export default postToApi;
